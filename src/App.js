fd35794391389db09fdb7aaa708969301708f6c0
PaavanKhunt <khuntpaavan2001@gmail.com>
2023-03-04 20:10:44 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+const urls = [
+  {
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    cover: 'https://i.ytimg.com/vi/aqz-KE-bpKQ/maxresdefault.jpg',
+  },
+  {
+    url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/339px-Big_buck_bunny_poster_big.jpg',
+  },
+];
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const ref = useRef();
+
+  const handleEnd = () => {
+    const total = urls.length;
+    if (currentIndex < total - 1) {
+      setCurrentIndex(currentIndex + 1);
+      ref.current.load();
+      ref.current.play();
+    }
+  };
+
+  const onMouseEnter = () => {
+    ref.current.play();
+  };
+
+  const onMouseLeave = () => {
+    ref.current.pause();
+  };
+
+  const handleVideoChange = (index) => {
+    setCurrentIndex(index);
+    ref.current.load();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        backgroundColor: 'black',
+      }}
+    >
+      <div>
+        <div
+          style={{
+            position: 'absolute',
+            right: 0,
+            zIndex: 100,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div>
+            <span style={{ color: 'white' }}>
+              {currentIndex + 1}/{urls.length}
+            </span>
+          </div>
+        </div>
+        <video
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+          ref={ref}
+          muted="muted"
+          style={{ height: 400, width: 400, objectFit: 'cover' }}
+          controls
+          onEnded={handleEnd}
+          poster={urls[currentIndex].cover}
+        >
+          <source src={urls[currentIndex].url} />
+        </video>
+        <div
+          style={{
+            position: 'absolute',
+            right: 0,
+            bottom: 12,
+            left: 0,
+            zIndex: 100,
+            paddingInline: 10,
+            paddingBlock: 12,
+            display: 'flex',
+            margin: 'auto',
+            justifyContent: 'space-evenly',
+            width: 60,
+          }}
+        >
+          {urls.map((_, index) => (
+            <button
+              onClick={() => handleVideoChange(index)}
+              disabled={index === currentIndex}
+              style={{
+                outline: 'none',
+                height: 12,
+                width: 12,
+                borderRadius: 6,
+                border: 'none',
+                cursor: 'pointer',
+                backgroundColor: index === currentIndex ? '#1e90ff' : 'white',
+              }}
+            ></button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
